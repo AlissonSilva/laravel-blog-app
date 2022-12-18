5eca0bd5e9a78484cdd8ec3c051f04574d2c1fa6
Alisson Oliveira <alisson_sao@hotmail.com>
2022-12-18 12:24:16 -0300

    Commit 2022-12-18
    Adicionado o Swagger Documentation relacionado a postagem.
    - Adicionado a documentação: Create, Update, Delete e Show unique post.

--- serve/app/Http/Controllers/PostController.php
+++ serve/app/Http/Controllers/PostController.php
@@ -8,6 +8,53 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+
+    /**
+     * Get post
+     * @OA\Get (
+     *     path="/api/posts/",
+     *     tags={"Post"},
+     *     security={{"Authorization": {}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="posts", type="object",
+     *                  @OA\Property(property="id", type="number", example=1),
+     *                  @OA\Property(property="user_id", type="number", example=1),
+     *                  @OA\Property(property="body", type="string", example="First Post!"),
+     *                  @OA\Property(property="image", type="files", example=null),
+     *                  @OA\Property(property="created_at", type="string", example="2022-12-11T09:25:53.000000Z"),
+     *                  @OA\Property(property="updated_at", type="string", example="2022-12-11T09:25:53.000000Z"),
+     *                  @OA\Property(property="comments_count", type="number", example=0),
+     *                  @OA\Property(property="likes_count", type="number", example=1),
+     *                  @OA\Property(property="user", type="object",
+     *                      @OA\Property(property="id", type="number", example=1),
+     *                      @OA\Property(property="name", type="string", example="Admin"),
+     *                      @OA\Property(property="image", type="files", example=null),
+     *                  ),
+     *                  @OA\Property(property="likes", type="array",
+     *                    @OA\Items(
+     *                         @OA\Property(property="id", type="number", example=1),
+     *                          @OA\Property(property="user_id", type="number", example=1),
+     *                          @OA\Property(property="post_id", type="number", example=1),
+     *                      ),
+     *                  ),
+     *              ),
+     * 
+     *          ),
+     * 
+     *     ),
+     * 
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      )
+     * )
+     */
     public function index()
     {
         return response([
@@ -20,6 +67,41 @@ class PostController extends Controller
         ], 200);
     }
 
+    /**
+     * Get unique post
+     * @OA\Get (
+     *     path="/api/posts/{id}",
+     *     tags={"Post"},
+     *     security={{"Authorization": {}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="post", type="array",
+     *                  @OA\Items(
+     *                      @OA\Property(property="id", type="number", example=2),
+     *                      @OA\Property(property="user_id", type="number", example=1),
+     *                      @OA\Property(property="body", type="string", example="First Post!"),
+     *                      @OA\Property(property="image", type="files", example=null),
+     *                      @OA\Property(property="created_at", type="string", example="2022-12-11T09:25:53.000000Z"),
+     *                      @OA\Property(property="updated_at", type="string", example="2022-12-11T09:25:53.000000Z"),
+     *                      @OA\Property(property="comments_count", type="number", example=0),
+     *                      @OA\Property(property="likes_count", type="number", example=1)
+     *                  ),
+     *              ),
+     * 
+     *          ),
+     *      ),
+     * 
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      )
+     * )
+     */
     public function show($id)
     {
         return response([
@@ -27,6 +109,58 @@ class PostController extends Controller
         ],200);
     }
 
+    /**
+     * Register new post
+     * @OA\Post (
+     *     path="/api/posts/",
+     *     tags={"Post"},
+     *     security={{"Authorization": {}}},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="body",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="image",
+     *                          type="file"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "body":"First Post!",
+     *                     "image":null
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Post created"),
+     *              @OA\Property(property="post", type="object",
+     *                  @OA\Property(property="body", type="string", example="First Post!"),
+     *                  @OA\Property(property="user_id", type="number", example=1),
+     *                  @OA\Property(property="image", type="files", example=null),
+     *                  @OA\Property(property="updated_at", type="string", example="2022-12-18T14:57:47.000000Z"),
+     *                  @OA\Property(property="created_at", type="string", example="2022-12-18T14:57:47.000000Z"),
+     *                  @OA\Property(property="id", type="number", example=2)
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      )
+     * )
+     */
     public function store(Request $request)
     {
         $attrs = $request->validate([
@@ -47,6 +181,53 @@ class PostController extends Controller
         ],200);
     }
 
+     /**
+     * Update post
+     * @OA\Put (
+     *     path="/api/posts/{id}",
+     *     tags={"Post"},
+     *     security={{"Authorization": {}}},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *         @OA\Schema(
+     *             @OA\Property(
+     *                  type="object",
+     *                  @OA\Property(
+     *                       property="body",
+     *                       type="string"
+     *                  )
+     *                 ),
+     *             example={
+     *                 "body":"First post! Show update!"
+     *              }
+     *            )
+     *         ),
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Post updated"),
+     *              @OA\Property(property="post", type="object",
+     *                  @OA\Property(property="id", type="number", example=2),
+     *                  @OA\Property(property="user_id", type="number", example=1),
+     *                  @OA\Property(property="body", type="string", example="First post! Show update!"),
+     *                  @OA\Property(property="image", type="files", example=null),
+     *                  @OA\Property(property="created_at", type="string", example="2022-12-18T14:57:47.000000Z"),
+     *                  @OA\Property(property="updated_at", type="string", example="2022-12-18T15:07:12.000000Z")
+     *              )
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      )
+     * )
+     */
     public function update(Request $request, $id)
     {
         $post = Post::find($id);
@@ -78,9 +259,9 @@ class PostController extends Controller
     }
 
     /**
-     * Delete Post
+     * Delete post
      * @OA\Delete (
-     *     path="/api/post/{id}",
+     *     path="/api/posts/{id}",
      *     tags={"Post"},
      *     security={{"Authorization": {}}},
      *     @OA\Parameter(
